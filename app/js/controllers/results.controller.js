d0a1a30a133d80f6ba536c59b5502e936ade5b9c
alexandr <alexandr.poddubnyi@ssa-outsourcing.com>
2016-12-20 13:50:12 +0200

    update

--- app/js/controllers/results.controller.js
+++ app/js/controllers/results.controller.js
@@ -14,8 +14,16 @@
     vm.quizMetrics = QuizMetrics;
     vm.data = DataService;
     vm.activeQuestion = 0;
+    vm.getAnswerClass = getAnswerClass;
     
     ////////////////
     
+    function getAnswerClass(index) {
+      if (index === vm.quizMetrics.correctAnswers[vm.activeQuestion]) {
+        return "bg-success";
+      } else if (index === vm.data.questions[vm.activeQuestion].selected) {
+        return "bg-danger";
+      }
+    }
   }
 })();
